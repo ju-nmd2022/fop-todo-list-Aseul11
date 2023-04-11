c1b64d4b0cac432ff3b585bd177377e47e2b4849
Aseul11 <asul22qa@student.ju.se>
2023-04-11 21:22:46 +0200

    final

--- toDo.js
+++ toDo.js
@@ -1,8 +1,8 @@
 // main source used for js part: https://www.youtube.com/watch?v=G0jO8kUrg-I
 
 // shows what will happen if 'add' button is clicked
-function addTaskButton() {
-  // if input box is empty, then alert pops up:
+function addNewTask() {
+  // if input field is empty, then alert pops up:
   if (taskInput.value === "") {
     alert("Error! The input field is empty!");
   } else {
@@ -10,6 +10,7 @@ function addTaskButton() {
     let li = document.createElement("li");
     li.innerHTML = taskInput.value;
     listContainer.appendChild(li);
+    // delete task button:
     let span = document.createElement("span");
     span.innerHTML = "\u00D7";
     li.appendChild(span);
@@ -21,6 +22,7 @@ function addTaskButton() {
 }
 
 // when clicking the task it will be marked as finished (line-through); got an idea how to do that from this source: https://www.w3schools.com/howto/howto_js_todolist.asp
+// styling made in css
 listContainer.addEventListener(
   "click",
   function (ev) {
